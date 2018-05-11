ce4645684188676605240e7a402bc5932d15a1ba
Kevin Boyle <papershack@gmail.com>
2018-05-11 13:24:54 -0700

    write basic practice function with ternary operator

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+
+// below is a ternary operator ie: a>b ? -- it is the equivalent of an if statement
+function findBiggestFraction(){
+  a>b ? console.log("a: ", a) : console.log("b: ", b);
+}
+var a = 3/4;
+var b = 5/7;
+
+findBiggestFraction();
