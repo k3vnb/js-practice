0607ff7ed8c74f7186f1c38e2e6afac78e7c8784
Kevin Boyle <papershack@gmail.com>
2018-05-11 14:00:15 -0700

    make anonymous function reusable

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,7 @@
-var a = 5/7;
-var b = 18/25;
-
-var theBiggest = function(){
+var theBiggest = function(a, b){
   var result;
   a>b ? result = ["a", a] : result = ["b", b];
-  console.log(result);
+  return result;
 }
 
-theBiggest();
+console.log(theBiggest(7/9, 13/25));
